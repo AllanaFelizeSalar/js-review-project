f241e1eddde8bef8c961337e6f62944044c7aa01
AllanaFelizeSalar <99715654+AllanaFelizeSalar@users.noreply.github.com>
2025-01-31 13:40:15 +0800

    Added Todo CRUD Management project

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 // array to store todos
 let todos = [];
 
-// DOM elements
+// DOM Elements
 const todoForm = document.getElementById('todo-form');
-const todoInput = document.getElementId('todo-input');
+const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
 // function to render todos
 function renderTodos() {
-    todoList.innerHTML = ''; // Clear the list
+    todoList.innerHTML = '';// Clear the list
     todos.forEach((todo, index) => {
+        const li = document.createElement('li');
         li.className = 'todo-item';
         li.innerHTML = `
             <span>${todo}</span>
             <button onclick="editTodo(${index})">Edit</button>
-            <button onclick="deleteTodo(${index})">Delete</button>
+            <button onclik="deleteTodo(${index})">Delete</button>
         `;
         todoList.appendChild(li);
-    })
-}
+    });
+}    
 
-// function to add new todo
+// function to add a new todo
 function addTodo(event) {
-    event.preventDefault();
+    event.preventDefault(); //Prevent form submission
     const newTodo = todoInput.value.trim();
     if (newTodo) {
         todos.push(newTodo);
-        todoInput.value = '';
+        todoInput.value = ''; // Clear the input
         renderTodos();
     }
 }
 
 // function to edit a todo
 function editTodo(index) {
-    constupdatedTodo = prompt('Edit your todo:', todos[index]);
-    if (updatedTodo !==null) {
+    const updatedTodo = prompt('Edit your todo:',todos[todos[index]])
+    if (updatedTodo !== null) {
         todos[index] = updatedTodo.trim();
         renderTodos();
     }
@@ -43,12 +44,12 @@ function editTodo(index) {
 // function to delete a todo
 function deleteTodo(index) {
     if (confirm('Are you sure you want to delete this todo?')) {
-        todos.splice(index, 1);
+        todos,splice(index, 1);
         renderTodos();
     }
 }
 
-// event listeners
+// event Listeners
 todoForm.addEventListener('submit', addTodo);
 
 // initial render
